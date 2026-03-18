21b46d063883b13c3e3391e278285d55a6a8a453
fayponjp <fayponjp@gmail.com>
2026-03-18 15:04:20 +0800

    using hash history for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './style.css'
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, history: createHashHistory() })
 
 declare module '@tanstack/react-router' {
   interface Register {

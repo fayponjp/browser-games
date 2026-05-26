0e1acc9f82269dd4934028fb3b172248df791475
fayponjp <fayponjp@gmail.com>
2026-05-26 19:12:43 +0800

    Removing 'merged' tag logic updates
    - 'merged' tile property should now be updated properly to false for all tiles after processing

--- src/2048/Game2048.tsx
+++ src/2048/Game2048.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import type { Tile } from '../shared-utils/types-interfaces';
 import { genInitialTiles } from './2048.util';
-import { processDirection, tileVariants, valid2048InputsArr } from './types-2048';
+import {
+    processDirection,
+    tileVariants,
+    valid2048InputsArr,
+} from './types-2048';
 
 export default function Game2048() {
     const [tiles, setTiles] = useState<Tile[]>(genInitialTiles);
-    const [isProcessing, setIsProcessing] = useState(false);
 
     let board: React.ReactElement[] = [];
     for (let tile of tiles) {
@@ -24,90 +27,109 @@ export default function Game2048() {
 
     const inputHandler = (directionInput: string): void => {
         if (valid2048InputsArr.includes(directionInput)) {
-            setIsProcessing(true)
-            const direction = processDirection[directionInput];
-            if (
-                direction === 'Left'
-            ) {
-                let tilesRow = 0;
-                
-                const newTileGrid: Tile[] = [];
+            setTiles((prevTiles) => {
+                const direction = processDirection[directionInput];
+                if (direction === 'Left') {
+                    let tilesRow = 0;
 
-                //if LEFT, 0; 3
-                // const initIndex = direction === 'Left' ? 0 : 3;
+                    const newTileGrid: Tile[] = [];
+                    while (tilesRow <= 3) {
+                        const processRow = prevTiles.filter(
+                            (tile) => tile.x === tilesRow,
+                        );
 
-                while (tilesRow <= 3) {
-                    const processRow = tiles.filter(
-                        (tile) => tile.x === tilesRow
-                    );
+                        const valuedTiles: Tile[] = [];
+                        const nullTiles: Tile[] = [];
+                        processRow.forEach((tile) => {
+                            if (tile.value) {
+                                const newTile = { ...tile, y: valuedTiles.length };
+                                valuedTiles.push(newTile);
+                            } else {
+                                const newTile = {
+                                    ...tile,
+                                    y: processRow.length - (nullTiles.length + 1),
+                                };
+                                nullTiles.push(newTile);
+                            }
+                        });
 
-                    const valuedTiles: Tile[] = [];
-                    const nullTiles: Tile[] = [];
-                    processRow.forEach((tile) => {
-                        if (tile.value) {
-                            const newTile = {...tile, y: valuedTiles.length}
-                            valuedTiles.push(newTile)
-                        } else {
-                            const newTile = {...tile, y: processRow.length - (nullTiles.length + 1)}
-                            nullTiles.push(newTile);
-                        }
-                    });
+                        let prevTile: Tile | undefined = undefined;
+                        let currentTile: Tile | undefined = undefined;
+                        for (let i = 0; i < valuedTiles.length; i++) {
+                            currentTile = valuedTiles[i];
 
-                    let prevTile: Tile | undefined = undefined;
-                    let currentTile: Tile | undefined = undefined;
-                    for (let i = 0; i < valuedTiles.length; i++) {
-                        currentTile = valuedTiles[i];
+                            if (prevTile) {
+                                const currVal = currentTile.value;
+                                const prevVal = prevTile.value;
+                                const prevMerged = prevTile.merged;
+                                const valuesMatch = currVal === prevVal;
 
-                        if (prevTile) {
-                            const currVal = currentTile.value;    
-                            const prevVal = prevTile.value;
-                            const prevMerged = prevTile.merged;
-                            const valuesMatch = currVal === prevVal;
-
-                            // match, is not newly merged
-                            if (valuesMatch && !prevMerged) {
-                                valuedTiles[i - 1] = {...prevTile, value: prevTile!.value! * 2, merged: true}
-                                valuedTiles[i] = {...currentTile, value: undefined, merged: false}
-                            // don't match, prev is empty so element prior is merged
-                            } else if (!valuesMatch && !prevVal) {
-                                valuedTiles[i - 1] = {...prevTile, value: currentTile.value, merged: true}
-                                valuedTiles[i] = {...currentTile, value: undefined, merged: false}
+                                // match, is not newly merged
+                                if (valuesMatch && !prevMerged) {
+                                    valuedTiles[i - 1] = {
+                                        ...prevTile,
+                                        value: prevTile!.value! * 2,
+                                        merged: true,
+                                    };
+                                    valuedTiles[i] = {
+                                        ...currentTile,
+                                        value: undefined,
+                                        merged: false,
+                                    };
+                                    // don't match, prev is empty so element prior is merged
+                                } else if (!valuesMatch && !prevVal) {
+                                    valuedTiles[i - 1] = {
+                                        ...prevTile,
+                                        value: currentTile.value,
+                                        merged: true,
+                                    };
+                                    valuedTiles[i] = {
+                                        ...currentTile,
+                                        value: undefined,
+                                        merged: false,
+                                    };
+                                }
                             }
 
-                            // otherwise don't do anything
+                            prevTile = valuedTiles[i];
                         }
 
-                        prevTile = valuedTiles[i];
+                        nullTiles.sort((a, b) => a.y - b.y);
+                        newTileGrid.push(...valuedTiles, ...nullTiles);
+
+                        tilesRow++;
                     }
-                    // sort nullTiles by y
-                    nullTiles.sort((a, b) => a.y - b.y);
-                    newTileGrid.push(...valuedTiles, ...nullTiles);
 
-                    tilesRow++;
+                    newTileGrid.forEach(
+                        (tile) => tile.merged = false
+                    );
+
+                    return newTileGrid
+                } else if (direction === 'Up') {
+                    return prevTiles
                 }
-                
-                newTileGrid.forEach((tile) => tile = {...tile, merged: false});
 
-                setTiles(newTileGrid);
-
-            } else if (
-                direction === 'Up'
-            ) {
-
-            }
-
-            setIsProcessing(false);
+                return prevTiles
+            })
         }
     };
-    // useKeyhandler([], inputHandler);
+    const inputHandlerRef = useRef(inputHandler);
+    useEffect(() => {
+        inputHandlerRef.current = inputHandler;
+    });
 
     useEffect(() => {
-        document.addEventListener('keydown', (e: KeyboardEvent) => {
-            if (!isProcessing) {
-                inputHandler(e.key)
-            } 
-        });
+        const handleKeyInput = (e: KeyboardEvent) => {
+            inputHandler(e.key);
+        };
+        document.addEventListener('keydown', handleKeyInput);
+
+        return () => document.removeEventListener('keydown', handleKeyInput);
     }, []);
+
+    // useEffect(() => {
+    //     console.log(tiles)
+    // }, [tiles])
     return (
         <div className='font-[Rubik] font-bold h-full w-full flex items-center justify-center bg-gray-800'>
             <div className='grid grid-cols-4 grid-rows-4 rounded-2xl p-3 gap-3 bg-(--board-bg)'>

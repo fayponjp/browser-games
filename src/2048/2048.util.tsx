23f6ade9d7ec7e7f936ce6c1a73d51d8ad674ba7
fayponjp <fayponjp@gmail.com>
2026-05-27 20:11:19 +0800

    Horizontal input processing added
    - separated logic to util file
    - processing for Left/Right inputs added and tested

--- src/2048/2048.util.tsx
+++ src/2048/2048.util.tsx
@@ -29,7 +29,6 @@ const generateTileValue = () => {
     return rand > 0.2 ? 2 : 4;
 };
 
-
 export const genInitialTiles = () => {
     const initCoords = initialCoords();
     const initTileBoard: Tile[] = [];
@@ -37,7 +36,7 @@ export const genInitialTiles = () => {
     const [firstSetX, firstSetY] = firstSet;
     const [secondSetX, secondSetY] = secondSet;
 
-    Array.from(Array(4).keys()).map((rowId) => (
+    Array.from(Array(4).keys()).map((rowId) =>
         Array.from(Array(4).keys()).map((colId) => {
             let initValue: number | undefined = undefined;
             // if (
@@ -47,22 +46,104 @@ export const genInitialTiles = () => {
             //     initValue = generateTileValue();
             // }
 
-            if (rowId === 0 && colId === 0) initValue = 2
-            if (rowId === 0 && colId === 1) initValue = 4
-            if (rowId === 1 && colId === 0) initValue = 8
-            if (rowId === 1 && colId === 1) initValue = 8
-            if (rowId === 1 && colId === 3) initValue = 16
-            if (rowId === 2 && colId === 2) initValue = 32
-            if (rowId === 2 && colId === 3) initValue = 64
-            if (rowId === 3 && colId === 3) initValue = 128
-            if (rowId === 3 && colId === 1) initValue = 256
-            if (rowId === 3 && colId === 0) initValue = 512
-            if (rowId === 0 && colId === 3) initValue = 1024
+            if (rowId === 0 && colId === 0) initValue = 2;
+            if (rowId === 0 && colId === 1) initValue = 4;
+            if (rowId === 1 && colId === 0) initValue = 8;
+            if (rowId === 1 && colId === 1) initValue = 8;
+            if (rowId === 1 && colId === 3) initValue = 16;
+            if (rowId === 2 && colId === 2) initValue = 32;
+            if (rowId === 2 && colId === 3) initValue = 64;
+            if (rowId === 3 && colId === 3) initValue = 128;
+            if (rowId === 3 && colId === 1) initValue = 256;
+            if (rowId === 3 && colId === 0) initValue = 512;
+            if (rowId === 0 && colId === 3) initValue = 1024;
             // if (rowId === 0 && colId === 3) initValue = 64
 
-            initTileBoard.push({x: rowId, y: colId, merged: false, value: initValue})
-        })
-    ));
+            initTileBoard.push({
+                x: rowId,
+                y: colId,
+                merged: false,
+                value: initValue,
+            });
+        }),
+    );
 
     return initTileBoard;
+};
+
+export const handleHorizontal = (tiles: Tile[], direction: string): Tile[] => {
+    let tilesRow = 0;
+
+    const newTileGrid: Tile[] = [];
+
+    while (tilesRow <= 3) {
+        // THIS BLOCK COULD BE GROUPING / SPLITTING
+        const processRow = tiles.filter((tile) => tile.x === tilesRow);
+        
+        const valuedTilesQty = processRow.filter((tile) => tile.value).length;
+        const nullTilesQty = processRow.filter((tile) => !tile.value).length;
+        const valuedTiles: Tile[] = [];
+        const nullTiles: Tile[] = [];
+
+        processRow.forEach((tile) => {
+            if (tile.value) {
+                valuedTiles.push({ ...tile, y: direction === 'Left' ? valuedTiles.length : nullTilesQty + valuedTiles.length});
+            } else {
+                nullTiles.push({ ...tile, y: direction === 'Left' ? valuedTilesQty + nullTiles.length : nullTiles.length});
+            }
+        });
+
+        // THIS BLOCK IS PROCESSING / COMPARING
+
+        if (direction === 'Right') valuedTiles.sort((a, b) => b.y - a.y);
+
+        let prevTile: Tile | undefined = undefined;
+        let currentTile: Tile | undefined = undefined;
+
+        for (let i = 0; i < valuedTiles.length; i++) {
+            currentTile = valuedTiles[i];
+
+            if (prevTile) {
+                const currVal = currentTile.value;
+                const prevVal = prevTile.value;
+                const prevMerged = prevTile.merged;
+                const valuesMatch = currVal === prevVal;
+
+                // match, is not newly merged
+                if (valuesMatch && !prevMerged) {
+                    valuedTiles[i - 1] = {
+                        ...prevTile,
+                        value: prevTile!.value! * 2,
+                        merged: true,
+                    };
+                    valuedTiles[i] = {
+                        ...currentTile,
+                        value: undefined,
+                        merged: false,
+                    };
+                    // don't match, prev is empty so element prior is merged
+                } else if (!valuesMatch && !prevVal) {
+                    valuedTiles[i - 1] = {
+                        ...prevTile,
+                        value: currentTile.value,
+                        merged: true,
+                    };
+                    valuedTiles[i] = {
+                        ...currentTile,
+                        value: undefined,
+                        merged: false,
+                    };
+                }
+            }
+
+            prevTile = valuedTiles[i];
+        }
+
+        if (direction === 'Right') valuedTiles.sort((a, b) => a.y - b.y);
+
+        const conditionalGrid = (direction === 'Left') ? [...valuedTiles, ...nullTiles] : [...nullTiles, ...valuedTiles]
+        newTileGrid.push(...conditionalGrid)
+        tilesRow++;
+    }
+    return newTileGrid;
 };
--- src/2048/Game2048.tsx
+++ src/2048/Game2048.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 
 import type { Tile } from '../shared-utils/types-interfaces';
-import { genInitialTiles } from './2048.util';
+import { genInitialTiles, handleHorizontal } from './2048.util';
 import {
     processDirection,
     tileVariants,
@@ -19,6 +19,7 @@ export default function Game2048() {
                 className={`h-26 w-26 flex rounded-lg ${tile.value ? `${tileVariants[tile.value]} shadow-sm shadow-black/50` : 'bg-(--board-card-null) inset-shadow-xs inset-shadow-black/75'}`}
             >
                 <span className='m-auto'>{tile.value}</span>
+                <p className='text-sm absolute'>{tile.x},{tile.y}</p>
             </div>
         );
 
@@ -29,87 +30,12 @@ export default function Game2048() {
         if (valid2048InputsArr.includes(directionInput)) {
             setTiles((prevTiles) => {
                 const direction = processDirection[directionInput];
-                if (direction === 'Left') {
-                    let tilesRow = 0;
-
-                    const newTileGrid: Tile[] = [];
-                    while (tilesRow <= 3) {
-                        const processRow = prevTiles.filter(
-                            (tile) => tile.x === tilesRow,
-                        );
-
-                        const valuedTiles: Tile[] = [];
-                        const nullTiles: Tile[] = [];
-                        processRow.forEach((tile) => {
-                            if (tile.value) {
-                                const newTile = { ...tile, y: valuedTiles.length };
-                                valuedTiles.push(newTile);
-                            } else {
-                                const newTile = {
-                                    ...tile,
-                                    y: processRow.length - (nullTiles.length + 1),
-                                };
-                                nullTiles.push(newTile);
-                            }
-                        });
-
-                        let prevTile: Tile | undefined = undefined;
-                        let currentTile: Tile | undefined = undefined;
-                        for (let i = 0; i < valuedTiles.length; i++) {
-                            currentTile = valuedTiles[i];
-
-                            if (prevTile) {
-                                const currVal = currentTile.value;
-                                const prevVal = prevTile.value;
-                                const prevMerged = prevTile.merged;
-                                const valuesMatch = currVal === prevVal;
-
-                                // match, is not newly merged
-                                if (valuesMatch && !prevMerged) {
-                                    valuedTiles[i - 1] = {
-                                        ...prevTile,
-                                        value: prevTile!.value! * 2,
-                                        merged: true,
-                                    };
-                                    valuedTiles[i] = {
-                                        ...currentTile,
-                                        value: undefined,
-                                        merged: false,
-                                    };
-                                    // don't match, prev is empty so element prior is merged
-                                } else if (!valuesMatch && !prevVal) {
-                                    valuedTiles[i - 1] = {
-                                        ...prevTile,
-                                        value: currentTile.value,
-                                        merged: true,
-                                    };
-                                    valuedTiles[i] = {
-                                        ...currentTile,
-                                        value: undefined,
-                                        merged: false,
-                                    };
-                                }
-                            }
-
-                            prevTile = valuedTiles[i];
-                        }
-
-                        nullTiles.sort((a, b) => a.y - b.y);
-                        newTileGrid.push(...valuedTiles, ...nullTiles);
-
-                        tilesRow++;
-                    }
-
-                    newTileGrid.forEach(
-                        (tile) => tile.merged = false
-                    );
-
-                    return newTileGrid
-                } else if (direction === 'Up') {
+                console.log(direction)
+                if (direction === 'Left' || direction === 'Right') {
+                    return handleHorizontal(prevTiles, direction);
+                } else {
                     return prevTiles
                 }
-
-                return prevTiles
             })
         }
     };
@@ -127,9 +53,9 @@ export default function Game2048() {
         return () => document.removeEventListener('keydown', handleKeyInput);
     }, []);
 
-    // useEffect(() => {
-    //     console.log(tiles)
-    // }, [tiles])
+    useEffect(() => {
+        console.log(tiles)
+    }, [tiles])
     return (
         <div className='font-[Rubik] font-bold h-full w-full flex items-center justify-center bg-gray-800'>
             <div className='grid grid-cols-4 grid-rows-4 rounded-2xl p-3 gap-3 bg-(--board-bg)'>

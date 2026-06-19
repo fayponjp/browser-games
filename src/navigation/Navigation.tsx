6a658916060a16f132fdce14a8e793ab26beb565
fayponjp <fayponjp@gmail.com>
2026-06-19 09:26:46 +0800

    Navigation styling updates
    - nav button color gradient brighter at the center relative to screen
    - padding adjustments for mobile/larger screens
    - nav bar color respects light/dark mode preference settings

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -9,15 +9,15 @@ export const NavigationPopover = () => {
         if (game.name) return <NavigationLink key={game.name} route={GameRoute[game.name]} >{game.title}</NavigationLink>
     });
     return (
-        <div className='relative m-auto w-full flex flex-row justify-between items-center max-w-5xl px-3 py-1'>
+        <div className='relative m-auto w-full flex flex-row justify-between items-center max-w-5xl px-3 lg:px-0 py-1'>
             <button
                 popoverTarget='navPopover'
                 className='font-mono gradient-text flex-row lg:ml-0 flex gap-2 [anchor-name:--nav-anchor] cursor-pointer hover:shadow-[inset_0_-2px_0_0_var(--theme-color)] transition-colors ease-in-out'
             >
+                <span className='lg:text-3xl text-6xl'>&#9776;</span>
                 <span className='text-2xl hidden lg:block'>
                     MENU
                 </span>{' '}
-                <span className='lg:text-3xl text-6xl'>&#9776;</span>
             </button>
             <div
                 id='navPopover'
@@ -28,7 +28,7 @@ export const NavigationPopover = () => {
                     {navigationLinks}
                 </nav>
             </div>
-            <Link to='/' className='gradient-text text-2xl'>HOME</Link>
+            <Link to='/' className='gradient-text-reverse text-2xl'>HOME</Link>
         </div>
     );
 };
@@ -39,7 +39,7 @@ export const NavigationBar = () => {
     const isHome = location.pathname === '/';
 
     return (
-        <div className={`w-full bg-(--theme-dark) ${isHome ? 'hidden' : ''}`}>
+        <div className={`w-full dark:bg-(--game-card-bg-dark) bg-(--game-card-bg-light) ${isHome ? 'hidden' : ''}`}>
             <NavigationPopover />
             
         </div>
